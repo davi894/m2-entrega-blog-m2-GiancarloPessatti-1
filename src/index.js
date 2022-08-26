437255e046282286e361ddab10c7e7e82e1057eb
Davi Moisés <moisesdavi005@gmail.com>
2022-08-26 10:47:13 -0300

    cadastroLogin

--- src/index.js
+++ src/index.js
@@ -1,4 +1,28 @@
 
 
+const idemaillogin = document.querySelector("#idemaillogin")
+
+const senhalogar = document.querySelector("#senhalogar")
+
+const botaologar = document.querySelector("#botaologar")
 
 
+botaologar.addEventListener("click", (e) => {
+    console.log(e.target)
+})
+
+
+const senhacadastro = document.querySelector("#senhacadastro")
+
+const Nomecadastro = document.querySelector("#Nomecadastro")
+
+const idemailcadastro = document.querySelector("#idemailcadastro")
+
+const imagemcadastro = document.querySelector("#imagemcadastro")
+
+const botaocadastrar = document.querySelector("#botaocadastrar")
+
+botaocadastrar.addEventListener("click", (e) => {
+    console.log(e.target)
+    senhacadastro.value
+})
