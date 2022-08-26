5a77cba90e96c4423436108f1e022b625d73ac82
Davi Moisés <moisesdavi005@gmail.com>
2022-08-26 11:44:08 -0300

    valoresdoInput

--- src/index.js
+++ src/index.js
@@ -1,28 +1,33 @@
-
-
-const idemaillogin = document.querySelector("#idemaillogin")
-
-const senhalogar = document.querySelector("#senhalogar")
+import { ApiBlogKenzie } from "./js/api.js"
 
 const botaologar = document.querySelector("#botaologar")
 
-
 botaologar.addEventListener("click", (e) => {
+
+    const idemaillogin = document.querySelector("#idemaillogin").value
+    const senhalogar = document.querySelector("#senhalogar").value
+
     console.log(e.target)
+    console.log(idemaillogin, senhalogar)
+
+    ApiBlogKenzie.login()
 })
-
-
-const senhacadastro = document.querySelector("#senhacadastro")
-
-const Nomecadastro = document.querySelector("#Nomecadastro")
-
-const idemailcadastro = document.querySelector("#idemailcadastro")
-
-const imagemcadastro = document.querySelector("#imagemcadastro")
 
 const botaocadastrar = document.querySelector("#botaocadastrar")
 
 botaocadastrar.addEventListener("click", (e) => {
+    const senhacadastro = document.querySelector("#senhacadastro").value
+
+    const Nomecadastro = document.querySelector("#Nomecadastro").value
+
+    const idemailcadastro = document.querySelector("#idemailcadastro").value
+
+    const imagemcadastro = document.querySelector("#imagemcadastro").value
+
+
+    console.log(senhacadastro, Nomecadastro, idemailcadastro, imagemcadastro)
     console.log(e.target)
-    senhacadastro.value
+
+    await ApiBlogKenzie.cadastro()
+
 })
